23bcd24ecbfac7f7d04363bf97995d2c665709a8
Dylan Cherry <dylancherry98@gmail.com>
2021-08-16 17:30:46 -0400

    Solving conflicts.

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,15 @@ ActiveRecord::Schema.define(version: 2021_08_16_203425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "listings", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.boolean "available"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,14 +36,6 @@ ActiveRecord::Schema.define(version: 2021_08_16_203425) do
     t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    
-  create_table "listings", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.float "price"
-    t.boolean "available"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
