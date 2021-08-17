fa9b5d08c45f138532b9e6eb49a656e45e757844
Nadav Friedman <nadav.friedman@mail.mcgill.ca>
2021-08-17 11:51:21 -0400

    created empty methods in listings controller corresponding to needed actions in routes

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,29 @@
 class ListingsController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def my_listings
+
+  end
 end
