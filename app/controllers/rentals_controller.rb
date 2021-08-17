b88b31388a0090e75c2c6a9444f1f4e7df6c5f86
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-17 11:30:59 -0700

    Created the rentals controller with index and create actions, and with the private methods

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,42 @@
+class RentalsController < ApplicationController
+  before_action :set_rental, only: [:edit]
+  before_action :set_listing, except: [:index]
+  
+  def index
+    @rentals = Rental.all
+  end
+
+  def new
+    @rental = Rental.new
+  end
+  
+  def create
+    #TODO
+  end
+
+  def edit
+  end
+
+  def update
+    #TODO
+  end
+
+  def listing_rentals
+    #TODO
+  end
+
+  private
+
+  def set_rental
+    @rental = Rental.find(:id)
+  end
+
+  def set_listing
+    @listing = Listing.find(params[:listing_id])
+  end
+
+  def rental_params
+    params.require(@rental).permit(:from, :until, :status, :user_id, :listing_id)
+  end
+
+end
