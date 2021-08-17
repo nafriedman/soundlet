92451d75d083c456e130bda5e40239f6a3b2a2af
Nadav Friedman <nadav.friedman@mail.mcgill.ca>
2021-08-17 12:05:29 -0400

    created foundation for index, new, create and show methods in listings controller. Have not creating listing_params method yet

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,18 +1,23 @@
 class ListingsController < ApplicationController
   def index
-
+    @listings = Listing.all
   end
 
   def new
-
+    @listing = Listing.new
   end
 
   def create
-
+    @listing = Listing.new(listing_params)
+    if @listing.save!
+      redirect_to listing_path(@listing)
+    else
+      render :new
+    end
   end
 
   def show
-
+    @listing = Listing.find(params[:id])
   end
 
   def edit
@@ -25,5 +30,10 @@ class ListingsController < ApplicationController
 
   def my_listings
 
+  end
+
+  private
+
+  def listing_params
   end
 end
