d1a689999d60c41c6e5c0a567ffcfd254ef60bad
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-19 17:43:21 -0700

    added columns and modify the title

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -26,7 +26,8 @@ class RentalsController < ApplicationController
   end
   
   def listing_rentals
-    @rentals = policy_scope(Rental)
+    @rentals = policy_scope(Rental).joins(:listing).where(listing_id: params[:listing_id]) 
+    # User.joins(:posts).where({ "posts.published" => true })
     authorize(@rentals)
   end
 
